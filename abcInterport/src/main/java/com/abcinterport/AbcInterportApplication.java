fbbb0c13ac317067f2e32b6e20b4441fb830410d
byroncervantes <byroncervantes@gmail.com>
2020-12-03 17:55:26 -0600

    Commit Inicial con eclipse

---
+++ abcInterport/src/main/java/com/abcinterport/AbcInterportApplication.java
@@ -0,0 +1,13 @@
+package com.abcinterport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcInterportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcInterportApplication.class, args);
+	}
+
+}
---
+++ abcInterport/src/test/java/com/abcinterport/AbcInterportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcinterport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcInterportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
